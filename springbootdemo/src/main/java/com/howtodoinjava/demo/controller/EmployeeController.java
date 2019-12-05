b9a7b683a7d715181496c0fd9fdd273893910042
anibhat <ani.infonaut@gmail.com>
2019-12-05 22:55:01 +0530

    Update EmployeeController.java

--- springbootdemo/src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
+++ springbootdemo/src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.howtodoinjava.demo.model.Employee;
@@ -11,8 +12,16 @@ import com.howtodoinjava.demo.model.Employee;
 @RestController
 public class EmployeeController {
 	
-	@RequestMapping("/")
+	@RequestMapping(value = "/employee", method = RequestMethod.GET)
     public List<Employee> getEmployees() 
+    {
+		List<Employee> employeesList = new ArrayList<Employee>();
+		employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com"));
+		return employeesList;
+    }
+	
+		@RequestMapping(value = "/addemployee", method = RequestMethod.POST)
+    public List<Employee> addEmployees() 
     {
 		List<Employee> employeesList = new ArrayList<Employee>();
 		employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com"));
